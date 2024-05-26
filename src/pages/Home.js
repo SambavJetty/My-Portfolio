1d7240bea146b2a973e962e278d3edea131ec475
Jetty Sambav <sambavjetty@gmail.com>
2024-05-26 17:34:44 +0530

    material/icon

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,6 @@
-import LinkedInIcon from 'mui/icons-material/LinkedIn';
-import EmailIcon from 'mui/icons-material/Email';
-import GithubIcon from 'mui/icons-material/GitHub';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import EmailIcon from '@mui/icons-material/Email';
+import GithubIcon from '@mui/icons-material/GitHub';
 import React from 'react';
 import "../styles/Home.css";
 
